abc4b628f7d76d87bd233433143e71adf843d573
Alexandra Juravets <70714347+alexandra74470@users.noreply.github.com>
2020-12-03 00:05:13 +0300

    Add files via upload

---
+++ al.4.py
@@ -0,0 +1,14 @@
+p=('perimetrul primului lot')
+l=('latimea lotului al doilea')
+a=('latura primului lot')
+k=('lungimea lotului al doilea')
+t=('perimetrul lotului al doilea')
+p=42
+l=9
+s=('aria')
+n=42
+a=42/4
+s=a*a
+k=s/l
+t=2*l+2*k
+print(t)
